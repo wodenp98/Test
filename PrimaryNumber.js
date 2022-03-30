c796d3c073845936105736750b9152c9d459f26e
Paul VIGNERON <paulvigneron4698@gmail.Com>
2022-03-30 17:03:21 +0200

    Nombre primaire

---
+++ PrimaryNumber.js
@@ -0,0 +1,12 @@
+function isPrime(n){
+    if (n <= 1)
+        return false;
+
+    for (let i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+  
+    return true;
+}
+
+console.log(isPrime(29))
